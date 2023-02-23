6befd6afa80fa3e3b22cd64ae26f5223ad7fc836
Xavier <32859092+xavier7t@users.noreply.github.com>
2023-02-22 22:00:15 -0500

    Initial Commit

---
+++ DemoLazyVStack20230222/DemoLazyVStack20230222App.swift
@@ -0,0 +1,17 @@
+//
+//  DemoLazyVStack20230222App.swift
+//  DemoLazyVStack20230222
+//
+//  Created by Xavier on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoLazyVStack20230222App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
